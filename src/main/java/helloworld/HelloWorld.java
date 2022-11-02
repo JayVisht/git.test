0eb83c230c64d87b0ab72c6eb39dc55a48510278
JAYSHREE VISHT <106261614+JVisht@users.noreply.github.com>
2022-11-02 12:08:15 -0400

    first commit

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -1,2 +1,10 @@
-package helloworld;public class HelloWorld {
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+
 }
